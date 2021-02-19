a34b96a502a624dbb2dfd83b6be8edc563ec95e5
kdevEC2 <ksk.22005150@gmail.com>
2021-02-19 09:32:40 +0000

    02/19 http-socket-uwsgi-django error

--- zPM/settings.py
+++ zPM/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'zapp1',
 ]
 
 MIDDLEWARE = [
---
+++ zapp1/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import views
+
+urlpattarns = [
+        path('index/', views.indexfunction),
+        ]
